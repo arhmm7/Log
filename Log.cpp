396ed08da5eb35c841529312a1389d47209d522b
Arham <145904433+arhmm7@users.noreply.github.com>
2024-04-24 02:18:59 +0530

    Update Log.cpp

--- Log.cpp
+++ Log.cpp
@@ -8,52 +8,60 @@
 
 #include "Log.h"
 
+#define _CRT_SECURE_NO_WARNINGS
+
+
 Log::Log()
 {
 	hc = GetStdHandle(STD_OUTPUT_HANDLE);
-	log("Initialized Log!",2);
+	log("Initialized Log!", 2);
 }
 Log::~Log()
 {
-	log("Cleared Log!",2);
+	log("Cleared Log!", 2);
 }
 
 
-std::string Log::getCurrentTime() 
-	{
-	    auto currentTime = std::chrono::system_clock::now();
-	    auto currentTime_t = std::chrono::system_clock::to_time_t(currentTime);
-	    auto timeinfo = std::localtime(&currentTime_t);
-	    std::ostringstream oss;
-	    oss << std::put_time(timeinfo, "%H:%M:%S");
-	    return oss.str();
-	}	
-
-void Log::log(const char* inp , int lglvl)
+std::string Log::getCurrentTime()
 {
-	if(lglvl == LogLevels::LogLevelError)
+	auto cT = std::chrono::system_clock::now();
+	auto cT_t = std::chrono::system_clock::to_time_t(cT);
+	std::tm timeinfo;
+#ifdef _MSC_VER
+	localtime_s(&timeinfo, &cT_t);
+#else
+	localtime_r(&currentTime_t, &timeinfo);
+#endif
+	std::ostringstream oss;
+	oss << std::put_time(&timeinfo, "%H:%M:%S");
+	return oss.str();
+}
+
+void Log::log(const char* inp, int lglvl)
+{
+	if (lglvl == LogLevels::LogLevelError)
 	{
-		SetConsoleTextAttribute(hc,0x0c);
+		SetConsoleTextAttribute(hc, 0x0c);
 		std::cout << " ( " << getCurrentTime() << " ) [ Error ] : " << inp << std::endl;
-		SetConsoleTextAttribute(hc,0x0f);
+		SetConsoleTextAttribute(hc, 0x0f);
 	}
-	else if(lglvl == LogLevels::LogLevelWarn)
+	else if (lglvl == LogLevels::LogLevelWarn)
 	{
-		SetConsoleTextAttribute(hc,0x0e);
+		SetConsoleTextAttribute(hc, 0x0e);
 		std::cout << " ( " << getCurrentTime() << " ) [ Warning ] : " << inp << std::endl;
-		SetConsoleTextAttribute(hc,0x0f);
+		SetConsoleTextAttribute(hc, 0x0f);
 	}
-	else if(lglvl == LogLevels::LogLevelInfo)
+	else if (lglvl == LogLevels::LogLevelInfo)
 	{
-		SetConsoleTextAttribute(hc,0x0a);
+		SetConsoleTextAttribute(hc, 0x0a);
 		std::cout << " ( " << getCurrentTime() << " ) [ Information ] : " << inp << std::endl;
-		SetConsoleTextAttribute(hc,0x0f);
+		SetConsoleTextAttribute(hc, 0x0f);
 	}
-	else if(lglvl == LogLevels::LogLevelTrace)
+	else if (lglvl == LogLevels::LogLevelTrace)
 	{
-		SetConsoleTextAttribute(hc,0x0f);
+		SetConsoleTextAttribute(hc, 0x0f);
 		std::cout << " ( " << getCurrentTime() << " ) [ Message ] : " << inp << std::endl;
-		SetConsoleTextAttribute(hc,0x0f);
-	}	
+		SetConsoleTextAttribute(hc, 0x0f);
+	}
 }
 
